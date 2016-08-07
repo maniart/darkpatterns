a571c1e60e87864c628df5dfff486733de824857
maniart <mani.art@gmail.com>
2016-08-07 19:01:06 -0400

    Add more geometries

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ class BoilerplateScene extends React.Component {
   changeTexture = () => {
     const sources = ['#popup-1', '#popup-2', '#popup-3', '#popup-4', '#popup-5', '#popup-6'];
     let texture = sources[Math.floor(Math.random() * sources.length)];
-    console.log('__ changing texture to: ', texture);
+    console.warn('#changeTexture: changing texture to: ', texture);
     this.setState({
       texture
     });
@@ -35,9 +35,20 @@ class BoilerplateScene extends React.Component {
         <Entity light={{type: 'ambient', color: '#888'}}/>
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
 
+        <Entity geometry="primitive: cone; radiusBottom: 1; radiusTop: 0.1" material={{src: this.state.texture}}
+                onClick={this.changeTexture}
+                position="0 0 -2">
+        <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+        </Entity>
+
         <Entity geometry="primitive: box" material={{src: this.state.texture}}
                 onClick={this.changeTexture}
-                position="0 0 -5">
+                position="4 3 -5">
+        <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+        </Entity>
+        <Entity geometry="primitive: box" material={{src: this.state.texture}}
+                onClick={this.changeTexture}
+                position="1 2 -5">
         <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
         </Entity>
       </Scene>
